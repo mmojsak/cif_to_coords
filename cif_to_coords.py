99939a72c536f7520eeff2bc2867e94e40cc0800
Mateusz <mateuszmojsak@gmail.com>
2022-09-25 23:18:19 +0100

    testing starfile

--- cif_to_coords.py
+++ cif_to_coords.py
@@ -2,3 +2,11 @@ cif_path = input("Please give the path to your .cif: ")
 
 print(f"The path to your cif is: {cif_path}")
 
+#import starfile
+#df = starfile.read('tests/dataframes/test_AuCl4_L.cif')
+#df
+
+
+
+
+
